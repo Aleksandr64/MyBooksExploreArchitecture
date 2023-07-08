e5e8918cb39fa14d540851f7d4d15729610c33b6
Oleksandr-Proniuk <90622988+Aleksandr64@users.noreply.github.com>
2023-07-08 10:08:24 +0300

    Middleware Custom Exception

--- Application/CustomExceptions/CustomExceptionMiddleware.cs
+++ Application/CustomExceptions/CustomExceptionMiddleware.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.ViewModels;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,34 @@ namespace Application.CustomExceptions
         public CustomExceptionMiddleware(RequestDelegate next)
         {
             _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            httpContext.Response.ContentType = "application/json";
+
+            var responce = new ErrorVM()
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware",
+                Path = "path-goes-here"
+            };
+            return httpContext.Response.WriteAsync(responce.ToString());
+
         }
     }
 }
--- Application/CustomExceptions/ExceptionMiddlewareExtensions.cs
+++ Application/CustomExceptions/ExceptionMiddlewareExtensions.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Application.ViewModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.CustomExceptions
 {
-    internal class ExceptionMiddlewareExtensions
+    public static class ExceptionMiddlewareExtensions
     {
+        public static void ConfigureBuildInExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var contextRequest = context.Features.Get<IHttpRequestFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorVM()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Path = contextRequest.Path
+                        }.ToString());
+                    }
+                });
+            });
+        }
+
+        public static void ConfigureCustomInExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<CustomExceptionMiddleware>();
+        }
     }
 }
