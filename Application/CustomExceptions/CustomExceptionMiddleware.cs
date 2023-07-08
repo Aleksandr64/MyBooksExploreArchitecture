d6f195c865bb2bb4c74bce1af7e98a7535c57b06
Oleksandr-Proniuk <90622988+Aleksandr64@users.noreply.github.com>
2023-07-08 09:11:40 +0300

    Custom Middleware Exception

---
+++ Application/CustomExceptions/CustomExceptionMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CustomExceptions
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+    }
+}
